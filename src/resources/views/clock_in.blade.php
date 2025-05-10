af953b380c26ba6d0599d3d07014ec982bdd9b5a
rina <yitenglinai69@gmail.com>
2025-05-10 13:18:57 +0900

    出勤登録画面

--- src/resources/views/clock_in.blade.php
+++ src/resources/views/clock_in.blade.php
@@ -1,1 +1,23 @@
-出勤登録画面（一般ユーザー）
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/clock_in.css') }}">
+@endsection
+
+@section('content')
+<div class="main">
+    <div class="work__status">
+        出勤中
+    </div>
+    <div class="date">
+        2025年5月10日(土)
+    </div>
+    <div class="time">
+        08:00
+    </div>
+    <div class="time-log__button">
+        <div class="button attendance">退勤</div>
+        <div class="button break">休憩入</div>
+    </div>
+</div>
+@endsection
