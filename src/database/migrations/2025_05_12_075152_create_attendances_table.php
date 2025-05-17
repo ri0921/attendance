1f06975d25734fdcbfaf9d8c7682476cfe472431
rina <yitenglinai69@gmail.com>
2025-05-17 11:50:28 +0900

    ダミーデータ作成

--- src/database/migrations/2025_05_12_075152_create_attendances_table.php
+++ src/database/migrations/2025_05_12_075152_create_attendances_table.php
@@ -16,6 +16,7 @@ class CreateAttendancesTable extends Migration
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
             $table->time('clock_in')->nullable();
             $table->time('clock_out')->nullable();
             $table->timestamps();
