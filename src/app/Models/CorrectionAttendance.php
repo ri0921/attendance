4dade644ad3010c37a486826235ba600fb29bad3
rina <yitenglinai69@gmail.com>
2025-05-24 09:40:53 +0900

    勤怠詳細情報の取得

--- src/app/Models/CorrectionAttendance.php
+++ src/app/Models/CorrectionAttendance.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CorrectionRequest extends Model
+class CorrectionAttendance extends Model
 {
     use HasFactory;
 
