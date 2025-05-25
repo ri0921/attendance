7282f51e70e03ad89e78883c90653bf9c201c327
rina <yitenglinai69@gmail.com>
2025-05-25 12:41:58 +0900

    管理者用勤怠一覧の表示

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
 <div class="main">
-    <h2 class="title">2023年6月1日の勤怠</h2>
+    <h2 class="title">{{ $current->year }}年{{ $current->month }}月{{ $current-> day }}日の勤怠</h2>
     <div class="date-selector">
-        <a href="" class="arrow"><img class="arrow__image" src="{{ asset('img/left.png') }}">前日</a>
-        <div class="date"><img class="calendar__image" src="{{ asset('img/calendar.png') }}">2023/06/01</div>
-        <a href="" class="arrow">翌日<img class="arrow__image" src="{{ asset('img/right.png') }}"></a>
+        <a href="{{ url('/admin/attendance/list') }}?date={{ $prev_date }}" class="arrow"><img class="arrow__image" src="{{ asset('img/left.png') }}">前日</a>
+        <div class="date"><img class="calendar__image" src="{{ asset('img/calendar.png') }}">{{ \Carbon\Carbon::parse($current)->format('Y/m/d') }}</div>
+        <a href="{{ url('/admin/attendance/list') }}?date={{ $next_date }}" class="arrow">翌日<img class="arrow__image" src="{{ asset('img/right.png') }}"></a>
     </div>
     <table class="table">
         <tr class="table__row">
@@ -21,30 +21,30 @@
             <th class="table__header">合計</th>
             <th class="table__header">詳細</th>
         </tr>
-        <tr class="table__row">
-            <td class="table__detail">西 伶奈</td>
-            <td class="table__detail">09:00</td>
-            <td class="table__detail">18:00</td>
-            <td class="table__detail">1:00</td>
-            <td class="table__detail">8:00</td>
-            <td class="table__detail"><a class="detail__link" href="/attendance/{id}">詳細</a></td>
-        </tr>
-        <tr class="table__row">
-            <td class="table__detail">西 伶奈</td>
-            <td class="table__detail">09:00</td>
-            <td class="table__detail">18:00</td>
-            <td class="table__detail">1:00</td>
-            <td class="table__detail">8:00</td>
-            <td class="table__detail"><a class="detail__link" href="/attendance/{id}">詳細</a></td>
-        </tr>
-        <tr class="table__row">
-            <td class="table__detail">西 伶奈</td>
-            <td class="table__detail">09:00</td>
-            <td class="table__detail">18:00</td>
-            <td class="table__detail">1:00</td>
-            <td class="table__detail">8:00</td>
-            <td class="table__detail"><a class="detail__link" href="/attendance/{id}">詳細</a></td>
-        </tr>
+        @isset($attendances)
+            @foreach ($attendances as $attendance)
+                <tr class="table__row">
+                    <td class="table__detail">
+                        {{ $attendance->user->name }}
+                    </td>
+                    <td class="table__detail">
+                        {{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') }}
+                    </td>
+                    <td class="table__detail">
+                        @isset($attendance->clock_out)
+                        {{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') }}
+                        @endisset
+                    </td>
+                    <td class="table__detail">
+                        {{ $attendance->total_break_formatted }}
+                    </td>
+                    <td class="table__detail">
+                        {{ $attendance->total_work_formatted }}
+                    </td>
+                    <td class="table__detail"><a class="detail__link" href="/attendance/{{ $attendance->id }}">詳細</a></td>
+                </tr>
+            @endforeach
+        @endisset
     </table>
 </div>
 @endsection
