21e318270d980b1251ebe13abc2ad4c8eec3a08d
rina <yitenglinai69@gmail.com>
2025-05-31 12:15:28 +0900

    全一般ユーザーの取得

--- src/app/Http/Controllers/Admin/StaffController.php
+++ src/app/Http/Controllers/Admin/StaffController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class StaffController extends Controller
 {
     public function index()
     {
-        return view('admin.staff');
+        $users = User::where('role', 'user')->get();
+        return view('admin.staff', compact('users'));
     }
 }
