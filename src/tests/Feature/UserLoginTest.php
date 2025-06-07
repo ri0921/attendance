dc8941cf322166a7723c150b57bbc54d588c0161
rina <yitenglinai69@gmail.com>
2025-06-08 08:49:42 +0900

    メールアドレス必須のバリデーションの確認

---
+++ src/tests/Feature/UserLoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Http\Middleware\VerifyCsrfToken;
+use Tests\TestCase;
+
+class UserLoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(DatabaseSeeder::class);
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
+
+    public function test_email_is_required()
+    {
+        $response = $this->post('/login', [
+                'email' => '',
+                'password' => 'password',
+            ]);
+        $response->assertRedirect();
+        $response->assertSessionHasErrors([
+            'email' => 'メールアドレスを入力してください']);
+    }
+}
