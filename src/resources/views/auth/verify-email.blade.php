00dafa67ee14b8e7882ee488507979b7f84e969c
rina <yitenglinai69@gmail.com>
2025-06-07 17:12:54 +0900

    メール認証の実装

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -5,15 +5,20 @@
 @endsection
 
 @section('content')
+@if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+@endif
 <div class="main">
     <div class="verify__message">
         <p>登録していただいたメールアドレスに認証メールを送付しました。</br>メール認証を完了してください。</p>
     </div>
     <div class="verify__button">
-        <button class="verify__button-submit">認証はこちらから</button>
+        <a class="verify__button-submit" href="https://mailtrap.io/inboxes">認証はこちらから</a>
     </div>
     <div class="resend-mail">
-        <form method="POST" action="/email/verification-notification">
+        <form method="post" action="/email/verification-notification">
             @csrf
             <button type="submit" class="resend-mail__link">
                 認証メールを再送する
