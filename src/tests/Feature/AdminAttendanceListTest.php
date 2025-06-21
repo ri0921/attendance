1f4b595a76e36c290715c3ab11f7cdd60d9c4e1b
rina <yitenglinai69@gmail.com>
2025-06-21 14:58:03 +0900

    前日の日付の勤怠情報が表示されることのテスト

--- src/tests/Feature/AdminAttendanceListTest.php
+++ src/tests/Feature/AdminAttendanceListTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Database\Seeders\DatabaseSeeder;
+use Database\Seeders\UsersTableSeeder;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Http\Middleware\VerifyCsrfToken;
 use Tests\TestCase;
@@ -18,7 +18,7 @@ class AdminAttendanceListTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->seed(DatabaseSeeder::class);
+        $this->seed(UsersTableSeeder::class);
         $this->withoutMiddleware(VerifyCsrfToken::class);
     }
 
@@ -50,5 +50,23 @@ class AdminAttendanceListTest extends TestCase
         $response = $this->get('/admin/attendance/list');
         $response->assertStatus(200);
         $response->assertSee($today->format('Y/m/d'));
+    }
+
+    public function test_previous_day_attendance()
+    {
+        $user = User::find(2);
+        $today = Carbon::today();
+        $prev_date = $today->copy()
+        ->subDay();
+        $attendance = Attendance::factory()->clocked_out()->create([
+            'user_id' => $user->id,
+            'date' => $prev_date,
+        ]);
+        $admin = User::find(1);
+        $this->actingAs($admin);
+        $response = $this->get("/admin/attendance/list?date={$prev_date->format('Y-m-d')}");
+        $response->assertStatus(200);
+        $response->assertSee($prev_date->format('Y/m/d'));
+        $response->assertSee($user->name);
     }
 }
